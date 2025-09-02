a2b5328f92e0d64b265bca7ca4677132b834349a
Jake Wilson <hi@jkwlsn.dev>
2025-09-02 11:33:20 +0100

    Adds Hello World class

---
+++ hello-world/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.print("This is a `print` statement.");
+        System.out.print("This is also a `print` statement, it should appear on the same line.");
+    }
+}
