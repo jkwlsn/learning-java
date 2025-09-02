58bc90884f5efef1481e7f3bb59b6c2a308c88e5
Jake Wilson <hi@jkwlsn.dev>
2025-09-02 11:43:52 +0100

    Function returns name and age from variables

---
+++ age-name/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        String name = "Jake";
+        int age = 34;
+        System.out.print("Name: ");
+        System.out.println(name);
+        System.out.print("Age:  ");
+        System.out.println(age);
+    }
+}
