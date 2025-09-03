8ecc145298c40235867a7b2d3f67634c9a78865e
Jake Wilson <hi@jkwlsn.dev>
2025-09-03 15:21:11 +0100

    Accept user input with the Scanner package

---
+++ input-output/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter username:\t");
+        String username = input.nextLine();
+        System.out.println("Username:\t" + username);
+    }
+}
