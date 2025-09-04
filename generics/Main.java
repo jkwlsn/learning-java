917020a5f8aa2598de6c9c123fa6c785516e935c
Jake Wilson <hi@jkwlsn.dev>
2025-09-04 13:27:55 +0100

    Limit the types by extending existing types
    You cna limit the types allowed by creating your custom type as an
    extension of an existing type. In this example, the Stats type is an
    extension of Number. Meaning I wouldn't be able to crate String types,
    but I can create Doubles or Integers or Shorts.

--- generics/Main.java
+++ generics/Main.java
@@ -7,5 +7,14 @@ public class Main {
         Box<Integer> numberBox = new Box<>();
         numberBox.set(999);
         System.out.println("numberBox\nValue:\t" + numberBox.get());
+
+        Double[] doubleNums = {1.1,1.2,1.3};
+        Stats<Double> doubleStats = new Stats<>(doubleNums);
+
+        System.out.print("Array: ");
+        for (Double dnum : doubleNums) {
+        System.out.print(dnum + "\t");
+        }
+        System.out.println("\nAverage:\t" + doubleStats.average());
     }
 }
